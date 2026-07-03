36f220e35c2d24a69d9212370e7b34aeed101f2a
Juan Putra Omartio <jpomartio08@gmail.com>
2026-07-03 10:37:53 +0700

    users done

--- backend-smartcost/src/modules/users/controller/interface.go
+++ backend-smartcost/src/modules/users/controller/interface.go
@@ -1,4 +1,17 @@
 package controller
 
+import (
+	"context"
+)
+
 type IController interface {
+	CreateUsers(ctx context.Context, req *CreateUserRequest) (res *CreateUserResponse, err error)
+
+	GetUsers(ctx context.Context, req *GetUsersRequest) (res *GetUsersResponse, err error)
+
+	GetUsersByID(ctx context.Context, req *GetUsersByIDRequest) (res *GetUsersByIDResponse, err error)
+
+	UpdateUsers(ctx context.Context, req *UpdateUserRequest) (res *UpdateUserResponse, err error)
+
+	DeleteUsers(ctx context.Context, req *DeleteUserRequest) (res *DeleteUserResponse, err error)
 }
